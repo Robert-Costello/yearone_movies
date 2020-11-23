2a918549cf43eec7436cd0ef7af5f2c6c2e2cc5f
Robert Costello <robertcostello81@gmail.com>
2020-11-22 21:43:11 -0500

    connected to firebase

---
+++ src/components/Firebase.js
@@ -0,0 +1,50 @@
+// import firebase from 'firebase';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyC4IjKfxn92xbl3axMAiT3RyzbuyQLHWHo',
+  authDomain: 'nosql-robert.firebaseapp.com',
+  databaseURL: 'https://nosql-robert.firebaseio.com',
+  projectId: 'nosql-robert',
+  storageBucket: 'nosql-robert.appspot.com',
+  messagingSenderId: '1033768236242',
+  appId: '1:1033768236242:web:a4d1a00efbdae2e1cda90b',
+  measurementId: 'G-NLZ4RVF2W3',
+};
+
+// //   // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+// const db = firebase.firestore();
+
+// let chats = db.collection('chats');
+
+// function getChats(callback) {
+//   this.unsub = this.chats
+//     .where('room', '==', 'gaming')
+//     .orderBy('created_at')
+//     .onSnapshot((snapshot) => {
+//       snapshot.docChanges().forEach((change) => {
+//         if (change.type === 'added') {
+//           // UPDATE UI
+//           callback(change.doc.data());
+//         }
+//       });
+//     });
+// }
+
+// getChats((data) => console.log(data));
+// console.log('PPPPP');
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/7.21.1/firebase-app.js"></script>
+
+// <script src="https://www.gstatic.com/firebasejs/7.21.1/firebase-firestore.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="https://www.gstatic.com/firebasejs/7.21.1/firebase-analytics.js"></script>
+
+// <script>
+//   // Your web app's Firebase configuration
+//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// </script>

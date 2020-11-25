5959b0e1b176552ec826e02c9dd1a8a879def7ef
Robert Costello <robertcostello81@gmail.com>
2020-11-25 13:08:19 -0500

    addded styling and search button

--- src/components/Search.js
+++ src/components/Search.js
@@ -63,7 +63,12 @@ export class Search extends Component {
       <div>
         <div className="header">
           <div>
-            <h1>YearOne Movie Search</h1>
+            <img
+              width="500px"
+              src="https://static.wixstatic.com/media/c93ed2_c57eae83724f4b9da0171370812ea7d8~mv2.png/v1/fill/w_766,h_118,al_c,q_85,usm_0.66_1.00_0.01/YearOne-Logo-WhiteYellow.webp"
+              alt="YearOne Movies"
+            ></img>
+            {/* <h1>YearOne Movies</h1> */}
           </div>
           <form className="search-form" onSubmit={this.handleSubmit}>
             <input
@@ -74,6 +79,18 @@ export class Search extends Component {
               autoComplete="off"
               onChange={this.handleChange}
             ></input>
+            <button className="search-button" onSubmit={this.handleSubmit}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                role="presentation"
+              >
+                <path d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0 .41-.41.41-1.08 0-1.49L15.5 14zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+              </svg>
+            </button>
           </form>
         </div>
 
